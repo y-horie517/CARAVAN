227d64acbf2a59b1fa61f6f135abb4c51866321f
Your Name <you@example.com>
2019-02-06 17:21:53 +0900

    [Start]4-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # １行追加するだけでindex ~ destroyまでの７つのルーティングを定義してくれます
+  resources :blogs
 end
